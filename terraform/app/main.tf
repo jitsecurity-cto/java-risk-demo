876ab769fc003eb1dc24a8a46cbf60622e9b3e7f
Isaac Shabtay <isaac-solvo@users.noreply.github.com>
2022-08-07 17:12:05 -0700

    WIP

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -354,8 +354,10 @@ resource "aws_instance" "instance" {
       "sudo apt-get -y update",
       "sudo apt-get -y upgrade",
       "echo \"Installing software\"",
-      "sudo apt-get -y install default-jre jetty9",
+      "sudo apt-get -y install default-jre jetty9 docker.io",
+      "sudo usermod -G docker ubuntu",
       "echo \"Software installed\"",
+      "sudo apt-get install liblog4j2-java=2.11.2-1",
       # Bug in the installer (doesn't add syslog to the jetty9 log)
       "sudo systemctl stop jetty9",
       "sudo chmod -R 777 /var/log/jetty9",
