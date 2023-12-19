632eb4ee70466bdf5eead50d6a9fc3eb392e8c50
david-solvo <66424923+david-solvo@users.noreply.github.com>
2023-12-19 18:01:13 +0200

    Update CartServlet.java

--- src/main/java/cloud/solvo/demo/risk/CartServlet.java
+++ src/main/java/cloud/solvo/demo/risk/CartServlet.java
@@ -3,6 +3,8 @@ package cloud.solvo.demo.risk;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import java.io.IOException;
 
