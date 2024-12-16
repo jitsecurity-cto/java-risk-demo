6192e8c364742beefc120e22eeff1574c8ae3078
Yonatan Greenfeld <100685375+YonatanGreenfeld@users.noreply.github.com>
2024-12-16 16:28:25 +0200

    Update secret.java

--- src/main/secret.java
+++ src/main/secret.java
@@ -1,4 +1,5 @@
 private final string MY_AWS_SECRET = "AKIAIOSFODNN7EXAMPLE"
 private final string MY_AWS_SECRET2 = "AKIAIOSFODNN7EXAMPLE"
 private final string MY_AWS_SECRET3 = "AKIAIOSFODNN7EXAMPLE"
+private final string MY_AWS_SECRET4 = "AKIAIOSFODNN7EXAMPLE"
 
